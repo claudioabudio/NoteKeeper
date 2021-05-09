3c1bb8a1c59f7568f765d088274f96c53e0406a0
claudio Abud <clau@gmail.com>
2021-05-08 23:11:08 -0500

    Pass extra from one activity to the other and retrieve the extra by using intent variable

--- NoteKeeper/app/src/main/java/com/example/notekeeper/MainActivity.kt
+++ NoteKeeper/app/src/main/java/com/example/notekeeper/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.notekeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var notePosition = POSITION_NOT_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +15,22 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val dm = DataManager()
-        val adapterCourses = ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item, dm.courses.values.toList())
+        val adapterCourses = ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item, DataManager.courses.values.toList())
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerCourses.adapter = adapterCourses
+
+
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        binding.editTextNoteTitle.setText(note.title)
+        binding.editTextNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        binding.spinnerCourses.setSelection(coursePosition)
     }
 }
--- NoteKeeper/app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ NoteKeeper/app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -2,19 +2,32 @@ package com.example.notekeeper
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.notekeeper.databinding.ActivityMainBinding
+import com.example.notekeeper.databinding.ActivityNoteListBinding
 
 class NoteListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNoteListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_list)
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             var activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+
+        binding.contentNoteListId.noteListId.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataManager.notes)
+        binding.contentNoteListId.noteListId.setOnItemClickListener { parent, view, position, id ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            activityIntent.putExtra(EXTRA_NOTE_POSITION, position)
             startActivity(activityIntent)
         }
     }
