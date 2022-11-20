4760be486ed2bf705f69087ed8915bacb3e75c1d
claudio Abud <clau@gmail.com>
2022-11-19 22:09:25 -0600

    use bindings instad findViewById() completed logic to calculate tip amount

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        if (cost == null) {
+            binding.tipAmount.text = ""
+            return
+        }
+        val percentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = percentage * cost
+        val roundUp = binding.roundupSwitch.isChecked
+        tip = if (roundUp) ceil(tip) else tip
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
     }
 }
